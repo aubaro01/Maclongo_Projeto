63c511cd9f5d6a3f9d73b2254f7de4524e2f9ec1
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2024-11-12 22:04:36 +0000

    Update nav.php

--- Templates/nav.php
+++ Templates/nav.php
@@ -1,1 +1,44 @@
+<? php 
+<nav>
+<div class="logo-name">
+        <div class="logo-image">
+            <img src="/img/logo.png" alt="LOGO ICON">
+        </div>
+        <span class="logo_name">MacDash</span>
+    </div>
 
+    <div class="menu-items">
+        <ul class="nav-links">
+            <li><a href="DashMac.php">
+                <i class="uil uil-estate"></i>
+                <span class="link-name">Home</span>
+            </a></li>
+            <li><a href="dash_contacts.php">
+                <i class="uil uil-users-alt"></i>
+                <span class="link-name">Contactos</span>
+            </a></li>
+            <li><a href="dash_produts.php">
+                <i class="uil uil-shopping-cart-alt"></i>
+                <span class="link-name">Produtos</span>
+            </a></li>
+        </ul>
+        
+        <ul class="logout-mode">
+            <li><a href="logout.php">
+                <i class="uil uil-signout"></i>
+                <span class="link-name">Logout</span>
+            </a></li>
+
+            <li class="mode">
+                <a href="#">
+                    <i class="uil uil-moon"></i>
+                    <span class="link-name">Modo Escuro</span>
+                </a>
+                <div class="mode-toggle">
+                    <span class="switch"></span>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+?>
