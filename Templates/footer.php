2ea5c90080d54d65d8db5cfde0d47e1804240dbc
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2024-11-12 22:05:38 +0000

    Update footer.php

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,1 +1,15 @@
+<? php
 
+<footer>
+    <div class="footer">
+        <div class="footer-icon">
+            <a href="https://www.facebook.com/people/Maclongo-Com%C3%A9rcio-de-M%C3%A1quinas-e-Ferramentas-Lda/100066271255190/"><i class="fab fa-facebook-f"></i></a>
+        </div>
+    <div class="footer-copy">
+        <p>MACLONGO, Lda | Todos os Direitos Reservados © 2024</p>
+    </div>
+    <p>By<a href="https://github.com/aubaro01">Aubaro</a></p>
+</footer>
+
+
+  ?>
