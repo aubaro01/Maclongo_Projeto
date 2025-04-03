2f2d5672c152fe00589436f53313123649ae317d
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-04-03 15:43:09 +0100

    up

--- Templates/navBar.php
+++ Templates/navBar.php
@@ -6,11 +6,11 @@
             <div class="bar"></div>
         </div>
         <ul id="nav-links">
-            <li><a href="/php/Produtos.php" onclick="toggleMenu()">Produtos</a></li>
-            <li><a href="/php/Servicos.php" onclick="toggleMenu()">Serviços</a></li>
-            <li><a href="/php/Marcas.php" onclick="toggleMenu()">Marcas</a></li>
-            <li><a href="/php/Empresa.php" onclick="toggleMenu()">Empresa</a></li>
+            <li><a href="Produtos.php" onclick="toggleMenu()">Produtos</a></li>
+            <li><a href="Servicos.php" onclick="toggleMenu()">Serviços</a></li>
+            <li><a href="Marcas.php" onclick="toggleMenu()">Marcas</a></li>
+            <li><a href="Sobre.php" onclick="toggleMenu()">Empresa</a></li>
             <li><a href="#contactos" onclick="toggleMenu()">Contactos</a></li>
-            <li><a href="/php/login.php" class="login-btn" onclick="toggleMenu()">Login</a></li> 
+            <li><a href="../Dashboard/Auth/login.php" class="login-btn" onclick="toggleMenu()">Login</a></li> 
         </ul>
     </nav>
